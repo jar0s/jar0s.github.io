2d25190e550e11dae2002943022901ce64121fc7
Jarosław Marek <jaroslaw.marek@gmail.com>
2021-05-03 22:37:42 +1200

    Test techradar

---
+++ precache-manifest.f2d53e8192b38afe33ca5fee58c4a4b9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db37a8390cfc71b7db6a8e829ef729ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b920af7f3ae37fb827be",
+    "url": "/static/css/main.93f39944.chunk.css"
+  },
+  {
+    "revision": "900f07dda4d6d2e256a0",
+    "url": "/static/js/2.4463f371.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.4463f371.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b920af7f3ae37fb827be",
+    "url": "/static/js/main.8ef184a0.chunk.js"
+  },
+  {
+    "revision": "25350bf97e1b6558d877",
+    "url": "/static/js/runtime-main.3b26f2f0.js"
+  },
+  {
+    "revision": "ad505a3fe95c6cfbeb543d6a5f98683c",
+    "url": "/static/media/back.ad505a3f.svg"
+  },
+  {
+    "revision": "37f3a7a3cbdda6377714783ef69322aa",
+    "url": "/static/media/blip_changed.37f3a7a3.svg"
+  },
+  {
+    "revision": "a0e8b6f46af9d6faf35cf52c0cf0d771",
+    "url": "/static/media/blip_default.a0e8b6f4.svg"
+  },
+  {
+    "revision": "3877e1c5a8de58f7467677a44699b011",
+    "url": "/static/media/blip_new.3877e1c5.svg"
+  },
+  {
+    "revision": "57f380344f320900f047e1b533515a35",
+    "url": "/static/media/clanot-news.57f38034.otf"
+  },
+  {
+    "revision": "2ecd8f6d9d0a28286596bc17b798bf10",
+    "url": "/static/media/clanot-thin.2ecd8f6d.otf"
+  },
+  {
+    "revision": "cb59b987cf02744b5b4bed311eb9dd91",
+    "url": "/static/media/close.cb59b987.svg"
+  },
+  {
+    "revision": "6630128a6cbc23317b0867cc2126fa17",
+    "url": "/static/media/overview.6630128a.svg"
+  },
+  {
+    "revision": "77b0097416fe2fb0afdd992c1f03254b",
+    "url": "/static/media/pie.77b00974.svg"
+  },
+  {
+    "revision": "b87af63540353a3abbadd1cbc9112413",
+    "url": "/static/media/question.b87af635.svg"
+  },
+  {
+    "revision": "ff8cf5f952d576c17b1eeeaf78d5c01b",
+    "url": "/static/media/search.ff8cf5f9.svg"
+  }
+]);
